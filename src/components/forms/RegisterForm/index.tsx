5fa976930ee8620adf304ce8b846caa35c42078a
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-10-19 12:27:04 +0300

    update: register form

--- src/components/forms/RegisterForm/index.tsx
+++ src/components/forms/RegisterForm/index.tsx
@@ -2,110 +2,99 @@
 import React from 'react';
 import Button from '@/components/buttons/Button';
 import Input from '@/components/forms/Input';
-import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import FormField from '../FormField';
 import PasswordInput from '../PasswordInput';
+import useForm from '@/hooks/useForm';
+import { FormValues } from './types';
 
-const validationSchema = Yup.object({
-  firstName: Yup.string().required('Required'),
-  lastName: Yup.string().required('Required'),
-  password: Yup.string()
-    .min(6, 'Minimal 6 symbols')
-    .nullable()
-    .required('field is empty'),
-  repeatPassword: Yup.string()
-    .min(6, 'Minimal 6 symbols')
-    .nullable()
-    .required('field is empty'),
-  email: Yup.string().email('Invalid email format').required('Required'),
-});
+const formFields: {
+  label: string;
+  field: keyof FormValues;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  component: React.ComponentType<any>;
+}[] = [
+  {
+    label: 'First name',
+    field: 'firstName',
+    component: Input,
+  },
+  {
+    label: 'Last name',
+    field: 'lastName',
+    component: Input,
+  },
+  {
+    label: 'Email',
+    field: 'email',
+    component: Input,
+  },
+  {
+    label: 'Password',
+    field: 'password',
+    component: PasswordInput,
+  },
+  {
+    label: 'Repeat password',
+    field: 'repeatPassword',
+    component: PasswordInput,
+  },
+];
 
 const RegisterForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      firstName: '',
-      lastName: '',
-      // countryCode: '',
-      // languageCode: '',
-      password: '',
-      repeatPassword: '',
-    },
-    onSubmit: (values) => {
-      console.log(values);
-      // alert(JSON.stringify(values, null, 2));
-    },
-    validationSchema,
-    validateOnBlur: true,
-  });
-
-  console.log(formik.errors);
+  const { getFieldProps, getFieldMeta, handleSubmit, isSubmitting, setErrors } =
+    useForm<FormValues>({
+      initialValues: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        repeatPassword: '',
+      },
+      validationSchema: Yup.object({
+        firstName: Yup.string().required('Required'),
+        lastName: Yup.string().required('Required'),
+        password: Yup.string()
+          .min(6, 'Minimal 6 symbols')
+          .nullable()
+          .required('field is empty'),
+        repeatPassword: Yup.string()
+          .min(6, 'Minimal 6 symbols')
+          .nullable()
+          .required('field is empty'),
+        email: Yup.string().email('Invalid email format').required('Required'),
+      }),
+      onSubmit: async (values) => {
+        const { firstName, lastName, email, password, repeatPassword } = values;
+        if (!email || !password || !repeatPassword || !firstName || !lastName) {
+          return;
+        }
+      },
+    });
 
   return (
     <div className="mx-auto p-6 md:p-12 md:max-w-[512px] w-full bg-white dark:bg-violet-500 rounded-xl">
       <h1 className="mb-10 font-bold text-3xl md:text-4xl text-center ">
         Sign up
       </h1>
-      <form className="flex flex-col gap-7" onSubmit={formik.handleSubmit}>
-        <FormField label="First Name">
-          <Input
-            name="firstName"
-            placeholder="First Name"
-            className="w-full"
-            onBlur={formik.handleBlur}
-            value={formik.values.firstName}
-            onChange={formik.handleChange}
-            error={formik.touched.firstName ? formik.errors.firstName : ''}
-          />
-        </FormField>
-        <FormField label="Last Name">
-          <Input
-            name="lastName"
-            placeholder="Last Name"
-            className="w-full"
-            value={formik.values.lastName}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            error={formik.touched.lastName ? formik.errors.lastName : ''}
-          />
-        </FormField>
-        <FormField label="Email">
-          <Input
-            name="email"
-            type="email"
-            placeholder="Email"
-            className="w-full"
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            error={formik.touched.email ? formik.errors.email : ''}
-          />
-        </FormField>
-        <FormField label="Password">
-          <PasswordInput
-            name="password"
-            placeholder="Password"
-            className="w-full"
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            error={formik.touched.password ? formik.errors.password : ''}
-          />
-        </FormField>
-        <FormField label="Repeat password">
-          <Input
-            name="repeatPassword"
-            placeholder="Repeat password"
-            className="w-full"
-            onBlur={formik.handleBlur}
-            value={formik.values.repeatPassword}
-            onChange={formik.handleChange}
-            error={
-              formik.touched.repeatPassword ? formik.errors.repeatPassword : ''
-            }
-          />
-        </FormField>
+      <form
+        className="flex flex-col gap-7"
+        onSubmit={(e) => {
+          if (isSubmitting) {
+            return;
+          }
+          handleSubmit(e);
+        }}
+      >
+        {formFields.map(({ field, component: Field, label }, idx) => (
+          <FormField label={label} key={idx} {...getFieldMeta(field)}>
+            <Field
+              className={'w-full'}
+              placeholder={label}
+              {...getFieldProps(field)}
+            />
+          </FormField>
+        ))}
         <Button
           as="button"
           uiColor="primary"
