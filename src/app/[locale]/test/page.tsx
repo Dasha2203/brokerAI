e5048bb9d244889071fd0b6f20e6f013bd1ac21d
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-11-07 02:04:02 +0300

    fix: build

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -1,12 +1,8 @@
-import React from "react"
-import '../../styles/global.css'
+import React from 'react';
+import '../../../styles/global.css';
 
 const Test = () => {
-  return (
-    <div className="text-violet-500">
-      test
-    </div>
-  )
+  return <div className="text-violet-500">test</div>;
 };
 
 export default Test;
