645fcae2bda241e45b7eefcb95d174c5b011a265
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-12-06 00:40:13 +0300

    feat: add icons

---
+++ src/icons/RightArrowIcon/index.tsx
@@ -0,0 +1,27 @@
+const RightArowIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="100%"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M12 18L18 12L12 6"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5 18L11 12L5 6"
+      stroke="currentColor"
+      strokeOpacity={0.1}
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default RightArowIcon;
