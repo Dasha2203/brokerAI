81641adde297728e95e23e16f9a51d955dfa66d1
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-12-08 23:32:29 +0300

    fix: PageContiner is flex element

--- src/components/ui/PageContainer/index.tsx
+++ src/components/ui/PageContainer/index.tsx
@@ -6,7 +6,10 @@ import clsx from 'clsx';
 const PageContainer = ({ className, style, children }: Props) => {
   return (
     <LayoutContainer
-      className={clsx('pt-30 md:pt-40 pb-20', className)}
+      className={clsx(
+        'pt-30 md:pt-40 pb-20 min-h-[100svh] flex flex-col',
+        className,
+      )}
       style={style}
     >
       {children}
